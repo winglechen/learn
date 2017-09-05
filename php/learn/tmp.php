d587c4db25b51443734a39c650f61a1558ae45e0
winglechen <winglechen@gmail.com>
2017-09-05 11:09:43 +0800

    some change

--- php/learn/tmp.php
+++ php/learn/tmp.php
@@ -1,16 +1,7 @@
 <?php
-function gen() {
-        $ret = (yield 'yield1');
-            var_dump($ret);
-                $ret = (yield 'yield2');
-                    var_dump($ret);
-}
 
-$gen = gen();
-var_dump('current',$gen->current());    // string(6) "yield1"
+$str = "Peters, Thomas J. and Robert H. Waterman, Jr.";
 
-$tmp = $gen->send('ret1');
-var_dump('current',$gen->current());
-// string(6) "yield2" (the var_dump of the ->send() return value)
-var_dump('send',$gen->send('ret2')); // string(4) "ret2"   (again from within gen)
-// NULL               (the return value of ->send())
+echo str_ireplace(",", '', $str);
+
+echo "\n";
