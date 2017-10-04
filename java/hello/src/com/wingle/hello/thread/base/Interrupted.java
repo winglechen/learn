a4daa68820c7d204c4cd2cabc9a758804b474490
思源 <winglechen@gmail.com>
2017-10-05 00:14:36 +0800

    add git ignore file

--- java/hello/src/com/wingle/hello/thread/base/Interrupted.java
+++ java/hello/src/com/wingle/hello/thread/base/Interrupted.java
@@ -1,4 +1,44 @@
 package com.wingle.hello.thread.base;
 
+import com.wingle.hello.thread.util.SleepUtil;
+
 public class Interrupted {
+
+    public static void main(String[] args) {
+        Thread sleepThread = new Thread(new SleepRunner(), "SleepRunner");
+        sleepThread.setDaemon(true);
+
+        Thread busyThread = new Thread(new BusyRunner(), "BusyRunner");
+        busyThread.setDaemon(true);
+
+        sleepThread.start();
+        busyThread.start();
+
+        SleepUtil.sleep(5);
+        sleepThread.interrupt();
+        busyThread.interrupt();
+
+        System.out.println("sleep thread state: " + sleepThread.isInterrupted());
+        System.out.println("busy thread state: " + busyThread.isInterrupted());
+
+        SleepUtil.sleep(5);
+    }
+
+    static class SleepRunner implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                SleepUtil.sleep(1);
+            }
+        }
+    }
+
+    static class BusyRunner implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                //doing nothing
+            }
+        }
+    }
 }
